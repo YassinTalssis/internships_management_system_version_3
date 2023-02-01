abd3e678180bcefb738e12e5ee8e95270b0146fd
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:41:23 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
