3293e5e7069331f4af2b7fe0957ed83404a54f0b
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:54:53 +0100

    Update UserDetailsServiceImpl.java

--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,11 +1,7 @@
 package com.example.demo.service;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.*;
-
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
  
