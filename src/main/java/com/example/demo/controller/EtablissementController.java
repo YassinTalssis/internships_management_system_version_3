928bd8241880e2d37c8b8d17d5339f02c93d0000
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:41:03 +0100

    Update EtablissementController.java

--- src/main/java/com/example/demo/controller/EtablissementController.java
+++ src/main/java/com/example/demo/controller/EtablissementController.java
@@ -1,10 +1,5 @@
 package com.example.demo.controller;
-
-
-
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.entity.Etablissement;
 import com.example.demo.service.EtablissementServiceImpl;
-
 
 @Controller
 public class EtablissementController {
