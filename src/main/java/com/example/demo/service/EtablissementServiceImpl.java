f48c2682d4a023832aff0b33ded421ebb64d673e
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:53:09 +0100

    Update EtablissementServiceImpl.java

--- src/main/java/com/example/demo/service/EtablissementServiceImpl.java
+++ src/main/java/com/example/demo/service/EtablissementServiceImpl.java
@@ -1,18 +1,8 @@
-
-
-
 package com.example.demo.service;
-
-
 import java.util.List;
-
-
-
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Etablissement;
 import com.example.demo.repository.EtablissementRepository;
-
 
 //import org.springframework.stereotype.Service;
 
