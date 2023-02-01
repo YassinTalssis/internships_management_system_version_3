7c591e7227904c6c101cd0ad6180a07986456899
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:46:08 +0100

    Update Student.java

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,12 +1,10 @@
 package com.example.demo.entity;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
