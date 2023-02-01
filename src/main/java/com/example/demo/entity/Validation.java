25e9f9f8e93164b8a94ada10391a3acd524cff5d
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:46:45 +0100

    Update Validation.java

--- src/main/java/com/example/demo/entity/Validation.java
+++ src/main/java/com/example/demo/entity/Validation.java
@@ -1,14 +1,11 @@
 package com.example.demo.entity;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @Data
