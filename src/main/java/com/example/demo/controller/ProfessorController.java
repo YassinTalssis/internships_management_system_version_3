8bbb3fa6f68cf72cec5d7f67fa106c3cac8d6477
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-02-01 23:42:03 +0100

    Update ProfessorController.java

--- src/main/java/com/example/demo/controller/ProfessorController.java
+++ src/main/java/com/example/demo/controller/ProfessorController.java
@@ -1,13 +1,9 @@
 package com.example.demo.controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.example.demo.service.StudentServiceImpl;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
