6a494cf957288c92a24f3431232971f64a1f541a
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-04-01 08:50:39 +0200

    Update ResponsibleService.java

--- src/main/java/com/example/demo/service/ResponsibleService.java
+++ src/main/java/com/example/demo/service/ResponsibleService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.Responsible;
 
-
+//talssis
 public interface ResponsibleService {
 	
 		List<Responsible> getAllResponsibles();	
