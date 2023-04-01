fc406452c639a91cf807fb42ec9eff106a194244
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-04-01 08:49:44 +0200

    Update EtablissementService.java

--- src/main/java/com/example/demo/service/EtablissementService.java
+++ src/main/java/com/example/demo/service/EtablissementService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.Etablissement;
-
+//talssis
 public interface EtablissementService {
 	List<Etablissement> getAllEtablissement();
 	
