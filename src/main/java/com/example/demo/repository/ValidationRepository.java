6a1001423e230c9cf1ac3cfdebf690f7c9378a36
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-04-21 16:34:13 +0000

    Update ValidationRepository.java

--- src/main/java/com/example/demo/repository/ValidationRepository.java
+++ src/main/java/com/example/demo/repository/ValidationRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.User;
-
+//yassin talssus
 public interface ValidationRepository extends JpaRepository<User, Integer> {
 
 }
