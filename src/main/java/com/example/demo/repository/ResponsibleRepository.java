c8e444d301461e3741d342d6483bf5a65ce6238f
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-04-21 16:33:20 +0000

    Update ResponsibleRepository.java

--- src/main/java/com/example/demo/repository/ResponsibleRepository.java
+++ src/main/java/com/example/demo/repository/ResponsibleRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Responsible;
-
+//yassin talssis
 public interface ResponsibleRepository extends JpaRepository<Responsible, Long>{
 
 }
