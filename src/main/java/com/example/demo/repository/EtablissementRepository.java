fbf74735cfb2a99ca97afc299c66b51312bc0a2c
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-04-21 16:33:04 +0000

    Update EtablissementRepository.java

--- src/main/java/com/example/demo/repository/EtablissementRepository.java
+++ src/main/java/com/example/demo/repository/EtablissementRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Etablissement;
 
-
+//yassin talssis
 
 public interface EtablissementRepository extends JpaRepository<Etablissement, Integer>{
 
